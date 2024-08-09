da8b149cc9a1dad58d0ddb4a368f6213b5dd0bd4
Wil220 <wilker.silva0714@gmail.com>
2024-08-09 01:56:03 -0300

    agregando navbar e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
 import Home from './pagina/Home/Home';
+import Navbar from './componets/navBar/NavBar';
+import Footer from './componets/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pagina/login/Login';
 
 function App() {
   return (
     <>
-    <Home/>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
 );
 }
--- src/pagina/Home/Home.tsx
+++ src/pagina/Home/Home.tsx
@@ -2,55 +2,29 @@ import React from 'react';
 import './Home.css';
 
 
-
 function Home() {
     return (
         <>
-
-            <div>
-                <h3 className='text-xl'>justify-content alinhamento horizontal</h3>
-                <h5>justify-start Esquerda</h5>
-                <div className="flex flex-row  justify-start m-3">
-                    <div className='basis-1/4 bg-red-500'>01</div>
-                    <div className='basis-1/4 bg-gray-400'>01</div>
-                </div>
-                <h5>justify-end Direita</h5>
-                <div className="flex flex-row  justify-end m-3">
-                    <div className='basis-1/4 bg-red-500'>01</div>
-                    <div className='basis-1/4 bg-gray-400'>01</div>
-                </div>
-                <h5>justify-center Centro</h5>
-                <div className="flex flex-row  justify-center m-3">
-                    <div className='basis-1/4 bg-red-500'>01</div>
-                    <div className='basis-1/4 bg-gray-400'>01</div>
-                </div>
-                <h5>justify-around estremidades</h5>
-                <div className="flex flex-row  justify-between m-3">
-                    <div className='basis-1/4 bg-red-500'>01</div>
-                    <div className='basis-1/4 bg-gray-400'>01</div>
-                </div>
-                <h5>justify-around estremidades com espaçamento</h5>
-                <div className="flex flex-row  justify-around m-3">
-                    <div className='basis-1/4 bg-red-500'>01</div>
-                    <div className='basis-1/4 bg-gray-400'>01</div>
-                </div>
-
+        <div className="bg-indigo-900 flex justify-center">
+          <div className='container grid grid-cols-2 text-white'>
+            <div className="flex flex-col gap-4 items-center justify-center py-4">
+              <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+  
+              <div className="flex justify-around gap-4">
+              
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+              </div>
             </div>
-
-            <div className='mt-12'>
-                <h3 className='text-xl'>justify-content alinhamento vertical</h3>
-                <div className='w-auto h-60 bg-gray-400 flex  justify-center items-start m-3'>
-                    <div className='basis-1/4 bg-red-500'>justify-center items-start alinhamento horizontal centro/vertical topo</div>
-                </div>
-                <div className='w-auto h-60 bg-gray-400 flex  justify-center items-center m-3'>
-                    <div className='basis-1/4 bg-red-500'>justify-center items-start alinhamento horizontal centro/vertical centro</div>
-                </div>
-                <div className='w-auto h-60 bg-gray-400 flex  justify-center items-end m-3'>
-                    <div className='basis-1/4 bg-red-500'>justify-center items-start alinhamento horizontal centro/vertical base</div>
-                </div>
+  
+            <div className="flex justify-center ">
+      
             </div>
-
-        </>
+          </div>
+        </div>
+      
+      </>
     );
 }
+
 export default Home;
